83a32c0b7b8b92161fe8fcf35e0204e79a606e46
guozhaohui <guozhaohui628@gmail.com>
2017-12-21 09:47:21 +0800

    2

--- app/src/main/java/guo/animalphawithscale/MainActivity.java
+++ app/src/main/java/guo/animalphawithscale/MainActivity.java
@@ -8,22 +8,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
-    private View view1;
+    private View view1,view2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        view1 =  this.findViewById(R.id.view1);
-        ObjectAnimator animAlpha = ObjectAnimator.ofFloat(view1, "alpha", 0.8f, 0.6f,0f);
+        view1 = this.findViewById(R.id.view1);
+        view2 = this.findViewById(R.id.view2);
+
+        startAnim(view1);
+        Timer timer = new Timer();
+        timer.schedule(task,400);
+
+    }
+
+
+    private void startAnim(View view){
+        ObjectAnimator animAlpha = ObjectAnimator.ofFloat(view, "alpha", 0.8f, 0f);
         animAlpha.setRepeatMode(ValueAnimator.RESTART);
         animAlpha.setRepeatCount(100);
-        ObjectAnimator animScaleX = ObjectAnimator.ofFloat(view1, "scaleX", 1f, 1.4f,1.6f);
+        ObjectAnimator animScaleX = ObjectAnimator.ofFloat(view, "scaleX", 1f, 1.5f);
         animScaleX.setRepeatMode(ValueAnimator.RESTART);
         animScaleX.setRepeatCount(100);
-        ObjectAnimator animScaleY = ObjectAnimator.ofFloat(view1, "scaleY", 1f, 1.4f,1.6f);
+        ObjectAnimator animScaleY = ObjectAnimator.ofFloat(view, "scaleY", 1f, 1.5f);
         animScaleY.setRepeatMode(ValueAnimator.RESTART);
         animScaleY.setRepeatCount(100);
         AnimatorSet animatorSet = new AnimatorSet();
@@ -31,4 +45,18 @@ public class MainActivity extends AppCompatActivity {
         animatorSet.setDuration(2000);
         animatorSet.start();
     }
+
+    private TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    startAnim(view2);
+                }
+            });
+
+        }
+    };
+
 }
